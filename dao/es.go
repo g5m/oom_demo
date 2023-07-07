8cd6b426bae80216cb793270dfef7919cd9c263b
guomiao <guomiao2@tal.com>
2023-07-07 16:20:46 +0800

    es

--- dao/es.go
+++ dao/es.go
@@ -2,25 +2,27 @@ package dao
 
 import (
 	"fmt"
-	"sync"
+	//"sync"
 
 	"github.com/olivere/elastic"
 )
 
 var esClient *elastic.Client
-var once sync.Once
+
+//var once sync.Once
 
 func NewEs() *elastic.Client {
-	once.Do(func() {
-		client, err := elastic.NewClient(
-			elastic.SetURL("123.57.167.85:9200"),
-			elastic.SetSniff(false),
-		)
-		if err != nil {
-			fmt.Println(err)
-		}
-		esClient = client
-	})
+	// once.Do(func() {
 
+	// })
+	client, err := elastic.NewClient(
+		elastic.SetURL("123.57.167.85:9200"),
+		elastic.SetSniff(false),
+	)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	esClient = client
 	return esClient
 }
